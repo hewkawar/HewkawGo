31c501814dc64dc9dcbc4bfed238b88f82abd904
HewkawAr <tanawat@hewkawar.xyz>
2025-06-02 14:08:21 +0700

    remove

--- README.md
+++
@@ -1,3 +0,0 @@
-# HewkawGo
-
-[![Deploy to Cloudflare](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/hewkawar/HewkawGo)
--- src/libs/site.ts
+++
@@ -1,8 +0,0 @@
-import { Site } from "@/types/interface";
-
-export function getSite(): Site {
-    return {
-        name: process.env.SITE_NAME,
-        url: process.env.SITE_URL,
-    }
-}
