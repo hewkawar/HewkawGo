2003e4335a8752c328e81f1e76926bf7fde5fa6f
HewkawAr <tanawat@hewkawar.xyz>
2025-06-02 11:38:53 +0700

    prepare Database

---
+++ src/app/api/route.ts
@@ -0,0 +1,17 @@
+import { getDB, initializeTable } from "@/libs/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const db = await getDB();
+    
+    await initializeTable(db);
+
+    const total = await db.prepare("SELECT id FROM links").all();
+
+    return NextResponse.json({ 
+        ok: true, 
+        static: {
+            total: total.results.length,
+        } 
+    });
+}
