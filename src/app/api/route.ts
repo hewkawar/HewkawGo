13e95d0b909754ba741464b312c430851e0f0b3e
HewkawAr <tanawat@hewkawar.xyz>
2025-06-02 11:52:26 +0700

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     const db = await getDB();
-    
+
     await initializeTable(db);
 
     const total = await db.prepare("SELECT id FROM links").all();
 
-    return NextResponse.json({ 
-        ok: true, 
+    return NextResponse.json({
+        ok: true,
         static: {
             total: total.results.length,
-        } 
+        }
     });
 }
